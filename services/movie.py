c25f1114e16cbae2077470ad88c7045644173a4e
Volodymyr Pankiv <ivovan4ik@gmail.com>
2024-11-05 14:46:28 +0200

    'solution'

---
+++ services/movie.py
@@ -0,0 +1,37 @@
+from db.models import Genre, Actor, Movie
+
+
+def get_movies(genres_ids: list[int] = None,
+               actors_ids: list[int] = None) -> Movie:
+    if not genres_ids and not actors_ids:
+        return Movie.objects.all()
+    if genres_ids and actors_ids:
+        return Movie.objects.filter(genres__in=genres_ids,
+                                    actors__in=actors_ids).distinct()
+    if genres_ids:
+        return Movie.objects.filter(genres__in=genres_ids)
+    if actors_ids:
+        return Movie.objects.filter(actors__in=actors_ids)
+
+
+def get_movie_by_id(id_: int) -> Movie:
+    return Movie.objects.filter(id=id_).first()
+
+
+def create_movie(movie_title: str,
+                 movie_description: str,
+                 genres_ids: list[int] = None,
+                 actors_ids: list[int] = None) -> Movie:
+    film = Movie.objects.create(
+        title=movie_title,
+        description=movie_description,
+    )
+
+    if genres_ids:
+        genres = Genre.objects.filter(id__in=genres_ids)
+        film.genres.add(*genres)
+    if actors_ids:
+        actors = Actor.objects.filter(id__in=actors_ids)
+        film.actors.add(*actors)
+
+    return film
---
+++ services/movie_session.py
@@ -0,0 +1,65 @@
+from django.core.exceptions import ObjectDoesNotExist
+from db.models import MovieSession, Movie, CinemaHall
+from datetime import datetime
+
+
+def create_movie_session(movie_show_time: datetime,
+                         movie_id: int,
+                         cinema_hall_id: int) -> MovieSession:
+    try:
+        movie = Movie.objects.get(id=movie_id)
+        cinema_hall = CinemaHall.objects.get(id=cinema_hall_id)
+    except ObjectDoesNotExist:
+        raise ValueError("Provided movie or cinema hall ID does not exist.")
+
+    return MovieSession.objects.create(
+        show_time=movie_show_time,
+        movie=movie,
+        cinema_hall=cinema_hall
+    )
+
+
+def get_movies_sessions(session_date: str = None) -> MovieSession:
+    if session_date:
+        try:
+            date_obj = datetime.strptime(session_date, "%Y-%m-%d").date()
+        except ValueError:
+            raise ValueError("Invalid date format. Expected 'year-month-day'")
+        return MovieSession.objects.filter(show_time__date=date_obj)
+    return MovieSession.objects.all()
+
+
+def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
+    return MovieSession.objects.filter(id=movie_session_id).first()
+
+
+def update_movie_session(session_id: int,
+                         show_time: datetime = None,
+                         movie_id: int = None,
+                         cinema_hall_id: int = None) -> bool:
+    try:
+        movie_session = MovieSession.objects.get(id=session_id)
+    except MovieSession.DoesNotExist:
+        return False
+
+    if show_time:
+        movie_session.show_time = show_time
+    if movie_id:
+        try:
+            movie_session.movie = Movie.objects.get(id=movie_id)
+        except Movie.DoesNotExist:
+            raise ValueError("Provided movie ID does not exist.")
+    if cinema_hall_id:
+        try:
+            movie_session.cinema_hall = CinemaHall.objects.get(
+                id=cinema_hall_id)
+        except CinemaHall.DoesNotExist:
+            raise ValueError("Provided cinema hall ID does not exist.")
+
+    movie_session.save()
+    return True
+
+
+def delete_movie_session_by_id(session_id: int) -> bool:
+    deleted, _ = MovieSession.objects.filter(id=session_id).delete()
+    return bool(deleted)
